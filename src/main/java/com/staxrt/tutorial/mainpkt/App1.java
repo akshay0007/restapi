cd10898a2eda67171e34212bede73b7358050567
ubuntu <omj.akshaypanwar@gmail.com>
2019-11-09 14:12:45 +0530

    add

---
+++ src/main/java/com/staxrt/tutorial/mainpkt/App1.java
@@ -0,0 +1,26 @@
+package com.staxrt.tutorial.mainpkt;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class App1 {
+
+    public static void main(String[] args) throws IOException {
+        App1 app = new App1();
+        Properties prop = app.loadPropertiesFile("db.properties");
+        prop.forEach((k, v) -> System.out.println(k + ":" + v));
+    }
+
+    public Properties loadPropertiesFile(String filePath) throws IOException {
+        Properties prop = new Properties();
+        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(filePath)) {
+            prop.load(resourceAsStream);
+        } catch (IOException e) {
+            System.err.println("Unable to load properties file : " + filePath);
+        }
+
+        return prop;
+
+    }
+}
--- src/main/java/com/staxrt/tutorial/model/User.java
+++ src/main/java/com/staxrt/tutorial/model/User.java
@@ -73,147 +73,147 @@ public class User {
     @LastModifiedBy
     private String updatedBy;
 
-  /**
-   * Gets id.
-   *
-   * @return the id
-   */
-  public long getId() {
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public long getId() {
         return id;
     }
 
-  /**
-   * Sets id.
-   *
-   * @param id the id
-   */
-  public void setId(long id) {
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(long id) {
         this.id = id;
     }
 
-  /**
-   * Gets first name.
-   *
-   * @return the first name
-   */
-  public String getFirstName() {
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
+    public String getFirstName() {
         return firstName;
     }
 
-  /**
-   * Sets first name.
-   *
-   * @param firstName the first name
-   */
-  public void setFirstName(String firstName) {
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-  /**
-   * Gets last name.
-   *
-   * @return the last name
-   */
-  public String getLastName() {
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
+    public String getLastName() {
         return lastName;
     }
 
-  /**
-   * Sets last name.
-   *
-   * @param lastName the last name
-   */
-  public void setLastName(String lastName) {
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-  /**
-   * Gets email.
-   *
-   * @return the email
-   */
-  public String getEmail() {
+    /**
+     * Gets email.
+     *
+     * @return the email
+     */
+    public String getEmail() {
         return email;
     }
 
-  /**
-   * Sets email.
-   *
-   * @param email the email
-   */
-  public void setEmail(String email) {
+    /**
+     * Sets email.
+     *
+     * @param email the email
+     */
+    public void setEmail(String email) {
         this.email = email;
     }
 
-  /**
-   * Gets created at.
-   *
-   * @return the created at
-   */
-  public Date getCreatedAt() {
+    /**
+     * Gets created at.
+     *
+     * @return the created at
+     */
+    public Date getCreatedAt() {
         return createdAt;
     }
 
-  /**
-   * Sets created at.
-   *
-   * @param createdAt the created at
-   */
-  public void setCreatedAt(Date createdAt) {
+    /**
+     * Sets created at.
+     *
+     * @param createdAt the created at
+     */
+    public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
     }
 
-  /**
-   * Gets created by.
-   *
-   * @return the created by
-   */
-  public String getCreatedBy() {
+    /**
+     * Gets created by.
+     *
+     * @return the created by
+     */
+    public String getCreatedBy() {
         return createdBy;
     }
 
-  /**
-   * Sets created by.
-   *
-   * @param createdBy the created by
-   */
-  public void setCreatedBy(String createdBy) {
+    /**
+     * Sets created by.
+     *
+     * @param createdBy the created by
+     */
+    public void setCreatedBy(String createdBy) {
         this.createdBy = createdBy;
     }
 
-  /**
-   * Gets updated at.
-   *
-   * @return the updated at
-   */
-  public Date getUpdatedAt() {
+    /**
+     * Gets updated at.
+     *
+     * @return the updated at
+     */
+    public Date getUpdatedAt() {
         return updatedAt;
     }
 
-  /**
-   * Sets updated at.
-   *
-   * @param updatedAt the updated at
-   */
-  public void setUpdatedAt(Date updatedAt) {
+    /**
+     * Sets updated at.
+     *
+     * @param updatedAt the updated at
+     */
+    public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
     }
 
-  /**
-   * Gets updated by.
-   *
-   * @return the updated by
-   */
-  public String getUpdatedBy() {
+    /**
+     * Gets updated by.
+     *
+     * @return the updated by
+     */
+    public String getUpdatedBy() {
         return updatedBy;
     }
 
-  /**
-   * Sets updated by.
-   *
-   * @param updatedBy the updated by
-   */
-  public void setUpdatedBy(String updatedBy) {
+    /**
+     * Sets updated by.
+     *
+     * @param updatedBy the updated by
+     */
+    public void setUpdatedBy(String updatedBy) {
         this.updatedBy = updatedBy;
     }
 
