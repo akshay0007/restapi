57be7e129016a02cc95bb9d36d8a1dbeef8bfdea
ubuntu <omj.akshaypanwar@gmail.com>
2020-05-16 20:39:16 +0530

    added

---
+++ src/test/java/com/staxrt/tutorial/loadcontext/JpaIntegrationConfig.java
@@ -0,0 +1,11 @@
+package com.staxrt.tutorial.loadcontext;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.staxrt.tutorial.loadcontext")
+public class JpaIntegrationConfig {
+}
